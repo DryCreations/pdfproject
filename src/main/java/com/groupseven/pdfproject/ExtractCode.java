d8fd1a93b30fbd4c33935080d12efcc2a07ae0f0
DESKTOP-8M6U7JG\Cassandra Mae <cassandramae99@gmail.com>
2021-03-11 16:29:51 -0500

    Method Headers For User Story 11

---
+++ src/main/java/com/groupseven/pdfproject/ExtractCode.java
@@ -0,0 +1,26 @@
+package com.groupseven.pdfproject;
+/**
+ *  
+ * @author Cassandra Mae
+ *
+ *
+ * 
+ * @{
+ *\defgroup Extract Extract code from the PDF
+ */
+/// Ref User_Story
+///
+public class ExtractCode {
+	/// Ref: 11.1
+	/// Ref: 11.2
+	public void getCodeFromPdf() {
+		///Ref: 11.3
+		extractTo(); 
+	}
+	/// Ref:11.3
+	public void extractTo() {
+		
+	}
+
+}
+/**@ */
