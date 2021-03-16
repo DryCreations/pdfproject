c7744343f047041c9c685f4779bf80fd8954b7ca
Parker L <43549673+leonard-parker5@users.noreply.github.com>
2021-03-16 13:00:31 -0400

    Method headers for User Story 3 and 10
    Method headers for User Story 3 and 10 and start of doxygen documentation comments.

---
+++ src/main/java/com/groupseven/pdfproject/MoveStack.java
@@ -0,0 +1,28 @@
+package com.groupseven.pdfproject;
+
+/**
+ *
+ * @author Parker Leonard
+ */
+/// \ref t10_2 "task 10.2"
+public class MoveStack <E> {
+
+    /// \ref t10_3 "task 10.3"
+    ///
+    public void push(E move){
+
+    }
+
+    /// \ref t10_3 "task 10.3"
+    ///
+    public E pop(){
+        return null;
+    }
+
+    /// \ref t10_3 "task 10.3"
+    ///
+    public E peek(){
+        return null;
+    }
+
+}
---
+++ src/main/java/com/groupseven/pdfproject/WordCharReader.java
@@ -0,0 +1,37 @@
+package com.groupseven.pdfproject;
+
+import java.io.File;
+
+/**
+ * @author Parker Leonard
+ */
+/// \ref t3_2 "task 3.2"
+public class WordCharReader {
+    private int wordcount;
+    private int charcount;
+    private File inputfile;
+
+    /// \ref t3_2 "task 3.2"
+    ///
+    public WordCharReader(File inputfile){
+
+    }
+
+    /// \ref t3_2 "task 3.2"
+    ///
+    public void readFile() {
+
+    }
+
+    /// \ref t3_2 "task 3.2"
+    ///
+    public int getCharCount() {
+        return charcount;
+    }
+
+    /// \ref t3_2 "task 3.2"
+    ///
+    public int getWordCount() {
+        return wordcount;
+    }
+}
