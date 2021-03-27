3792100363af9dbaa6f2f687226ff5eae48e69c7
Cassandra Mae <patel.855@wright.edu>
2021-03-27 17:09:45 -0400

    completion of Action 33

--- src/main/java/com/groupseven/pdfproject/App.java
+++ src/main/java/com/groupseven/pdfproject/App.java
@@ -16,6 +16,7 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -23,6 +24,13 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
+	
+
+	public static final int WINDOW_WIDTH = 700;
+    public static final int WINDOW_HEIGHT = 790;
+    
+    MainCanvas canvas = new MainCanvas();
+    
 
     DocumentModel doc;
 
@@ -51,10 +59,23 @@ public class App extends Application {
     ///
     /// \ref t14_1 "task 14.1"
     private VBox createViewbox() {
+    	
         PageModel page = doc.getPage(0);
         VBox vbox = new VBox(0);
         vbox.setAlignment(Pos.BASELINE_RIGHT);
-        vbox.getChildren().add(page.getNode());
+        
+       vbox.getChildren().add(page.getNode());
+        
+        vbox.getChildren().add(canvas);
+        
+//        vbox.setOnKeyPressed(new EventHandler<KeyEvent>() {
+//            @Override
+//            public void handle(KeyEvent event) {
+//                canvas.getEventHandler().Event(event);
+//            }
+//        });
+        
+        
         return vbox;
     }
 
@@ -167,19 +188,38 @@ public class App extends Application {
         GridPane ToolBox = createToolBox();
         MenuBar menuBar = createMenuBar();
         Button undobutton = createUndoButton();
-        Button redobutton = createRedoButton();
+        Button redobutton = createRedoButton(); 
+        
+        /// ref t18_1 "task 18.1"
+        /// The HBox SelectBox provides the option for Select Tool and the Shape
+        HBox selectBox =new HBox();
+        selectBox.getChildren().add(new ShapesToolBar(canvas));
+
 
         GridPane.setConstraints(undobutton, 0, 0);
         GridPane.setConstraints(redobutton, 1, 0);
+       
 
+        
         root.setTop(menuBar);
         root.setLeft(ToolBox);
         root.setCenter(viewbox);
+        root.setRight(selectBox);
+        
 
         ToolBox.getChildren().addAll(undobutton,redobutton);
 
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        primaryStage.setScene(scene);
+       
         primaryStage.show();
+        
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                canvas.getEventHandler().Event(event);
+            }
+        });
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/groupseven/pdfproject/SelectTool.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.groupseven.pdfproject;
+
+import javafx.geometry.Point2D;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+/**
+ * @author Cassandra Mae
+ *\defgroup Selection
+ *@{
+ *\brief Selects the shape that is drawn on the canvas.
+ *\ref t18_1 "Task 18.1"
+ */
+public class SelectTool implements HandlingEvents{
+	
+	private MainCanvas canvas;
+	private Shapes selectedShape;
+	private Point2D pressedPoint;
+	
+	///\ref t18_1 "task 18.1"
+	/// @param canvas the canvas to select the shape 
+	public SelectTool(MainCanvas canvas) {
+		this.canvas = canvas;
+	}
+	
+	/// \brief Selects the shape drawn on the canvas
+	/// \ref t18_1 "Task 18.1"
+	@Override
+	public void Event(MouseEvent event) {
+		Point2D currentPoint = new Point2D(event.getX(), event.getY());
+
+        if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+            pressedPoint = currentPoint;
+            selectedShape = null;
+            for (Shapes obj : canvas.getShapes())
+                if (obj.contains(currentPoint)) {
+                    selectedShape = obj;
+                    obj.setElementSelected(true);
+                } else
+                    obj.setElementSelected(false);
+
+}
+        
+
+        canvas.update();
+    }
+
+	/// \brief Removes the selected shape
+	/// \ref t18_1 "task 18.1"
+    @Override
+    public void Event(KeyEvent k_event) {
+        if (selectedShape != null && k_event.getCode() == KeyCode.DELETE || k_event.getCode() == KeyCode.BACK_SPACE) {
+            canvas.getShapes().remove(selectedShape);
+            canvas.update();
+        }
+    }
+
+}
+/**@}**/
