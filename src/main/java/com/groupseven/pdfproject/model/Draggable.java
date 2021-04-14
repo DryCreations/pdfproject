3385057cfb1f51f19b345abed4d93ca725ff2a3d
Hayden Mankin <DryCreations@gmail.com>
2021-04-13 20:08:20 -0400

    Revert "Revert "Action 47: Allow user to open existing documents""

--- src/main/java/com/groupseven/pdfproject/model/Draggable.java
+++ src/main/java/com/groupseven/pdfproject/model/Draggable.java
@@ -3,8 +3,7 @@ package com.groupseven.pdfproject.model;
 /**
  * @author Charles Witherspoon
  *
- * \brief This interface represents a draggable canvas element
- * \ref t8_6 "Task 8.6"
+ *         \brief This interface represents a draggable canvas element \ref t8_6 "Task 8.6"
  */
 public interface Draggable {
     Action dragTo(double x, double y);
--- src/main/java/com/groupseven/pdfproject/utilities/DrawRectangle.java
+++ src/main/java/com/groupseven/pdfproject/utilities/DrawRectangle.java
@@ -4,6 +4,8 @@ import com.groupseven.pdfproject.MainCanvas;
 import com.groupseven.pdfproject.model.Action;
 import com.groupseven.pdfproject.model.Draggable;
 import com.groupseven.pdfproject.model.Selectable;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import javafx.event.Event;
 import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
@@ -13,8 +15,8 @@ import javafx.scene.shape.Shape;
 
 /**
  * @author Charles Witherspoon
- * @{ \brief This class represents an action to draw a shape on the canvas
- * \ref t9_1 "Task 9.1"
+ * 
+ * @{ \brief This class represents an action to draw a shape on the canvas \ref t9_1 "Task 9.1"
  */
 public class DrawRectangle implements Action, Selectable, Draggable {
     private MainCanvas _canvas;
@@ -49,20 +51,14 @@ public class DrawRectangle implements Action, Selectable, Draggable {
         if (_origin == null)
             _origin = new Point2D(mouseEvent.getX(), mouseEvent.getY());
         else {
-            _rectangle = new Rectangle(
-                    _origin.getX(),
-                    _origin.getY(),
-                    mouseEvent.getX() - _origin.getX(),
+            _rectangle = new Rectangle(_origin.getX(), _origin.getY(), mouseEvent.getX() - _origin.getX(),
                     mouseEvent.getY() - _origin.getY());
             _rectangle.setFill(_color);
         }
 
         _isComplete = (mouseEvent.getEventType() == MouseEvent.MOUSE_RELEASED);
         if (_isComplete) {
-            _selectionOverlay = new Rectangle(
-                    _rectangle.getX() - 2,
-                    _rectangle.getY() - 2,
-                    _rectangle.getWidth() + 4,
+            _selectionOverlay = new Rectangle(_rectangle.getX() - 2, _rectangle.getY() - 2, _rectangle.getWidth() + 4,
                     _rectangle.getHeight() + 4);
             _selectionOverlay.setFill(Color.grayRgb(100, 0.2));
             _selectionOverlay.setOnMouseClicked(__ -> unselect());
@@ -87,7 +83,7 @@ public class DrawRectangle implements Action, Selectable, Draggable {
 
     @Override
     public void unselect() {
-       _selected = false;
+        _selected = false;
     }
 
     @Override
@@ -101,8 +97,7 @@ public class DrawRectangle implements Action, Selectable, Draggable {
             return false;
 
         DrawRectangle other = (DrawRectangle) o;
-        return this._rectangle.getX() == other._rectangle.getX()
-                && this._rectangle.getY() == other._rectangle.getY()
+        return this._rectangle.getX() == other._rectangle.getX() && this._rectangle.getY() == other._rectangle.getY()
                 && this._rectangle.getWidth() == other._rectangle.getWidth()
                 && this._rectangle.getHeight() == other._rectangle.getHeight();
     }
@@ -112,13 +107,18 @@ public class DrawRectangle implements Action, Selectable, Draggable {
         unselect();
         _moved = true;
         DrawRectangle drawRectangle = new DrawRectangle(_canvas, _color);
-        drawRectangle._rectangle = new Rectangle(
-                _rectangle.getX(),
-                _rectangle.getY(),
-                _rectangle.getWidth(),
+        drawRectangle._rectangle = new Rectangle(_rectangle.getX(), _rectangle.getY(), _rectangle.getWidth(),
                 _rectangle.getHeight());
         drawRectangle._rectangle.setFill(_color);
         return drawRectangle;
+    }
+
+    @Override
+    public void pdfExecute(PdfCanvas canvas, PdfPage page) {
+        // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose
+        // Tools | Templates.
+        PdfAction.DRAW_RECTANGLE.accept(canvas, page, _rectangle);
+        System.out.println("Rectangle");
     }
 }
 /**
