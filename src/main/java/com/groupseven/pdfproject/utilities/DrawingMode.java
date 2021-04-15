3d91f3ac6d5a4e062b9e1182064f876c5c91303f
Charles Witherspoon <charles.v.witherspoon@gmail.com>
2021-04-15 19:41:43 -0400

    Added JaCoCo code coverage functionality

--- src/main/java/com/groupseven/pdfproject/utilities/DrawingMode.java
+++ src/main/java/com/groupseven/pdfproject/utilities/DrawingMode.java
@@ -3,7 +3,8 @@ package com.groupseven.pdfproject.utilities;
 /**
  * @author Charles Witherspoon
  *
- *         \brief This enum represents the different drawing modes available \ref t9_1 "Task 9.1"
+ * \brief This enum represents the different drawing modes available
+ * \ref t9_1 "Task 9.1"
  */
 public enum DrawingMode {
     PEN, ERASER, RECTANGLE, SELECT;
