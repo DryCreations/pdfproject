032944b7b0f5544b2d683853ea52e21ee65ff9e6
Cassandra Mae <patel.855@wright.edu>
2021-04-16 16:05:20 -0400

    removed the line of code as discussed

--- src/main/java/com/groupseven/pdfproject/App.java
+++ src/main/java/com/groupseven/pdfproject/App.java
@@ -138,7 +138,7 @@ public class App extends Application {
                 canvas.getChildren().remove(_drawingToolBar);
             } else {
                 canvas.getChildren().add(_drawingToolBar);
-                _drawingToolBar.setLayoutY(50);
+
             }
 
         });
