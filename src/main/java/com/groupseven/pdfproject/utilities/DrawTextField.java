bab7a4c85e52047d80603364eb7085d29d208548
Hunter Gongloff <hgongloff12@gmail.com>
2021-04-20 20:54:24 -0400

    Fixed Formatting

--- src/main/java/com/groupseven/pdfproject/utilities/DrawTextField.java
+++ src/main/java/com/groupseven/pdfproject/utilities/DrawTextField.java
@@ -1,4 +1,5 @@
 package com.groupseven.pdfproject.utilities;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -23,136 +24,138 @@ import javafx.event.Event;
 import javafx.geometry.Point2D;
 import javafx.scene.input.MouseEvent;
 
-
 /**
  * @author Hunter Gongloff
  * 
- * @{ \brief This is the class to create a text field on user click
- *    \ref 16_1 "Task 16.1" 
+ * @{ \brief This is the class to create a text field on user click \ref 16_1 "Task 16.1"
  */
 public class DrawTextField implements Action {
 
-	public static final String SRC = "src/main/resources/manipulate_pdf/test_pdf.pdf";
-	public static final String DES = "src/main/resources/manipulate_pdf/test_pdf_old.pdf";
+    public static final String SRC = "src/main/resources/manipulate_pdf/test_pdf.pdf";
+    public static final String DES = "src/main/resources/manipulate_pdf/test_pdf_old.pdf";
 
-	/// \ref t16_1 "task 16.1"
-	public DrawTextField(MainCanvas _canvas) {
-		// TODO Auto-generated constructor stub
-	}
-	/// \ref t16_1 "task 16.1"
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
-	}
-	
+    /// \ref t16_1 "task 16.1"
+    public DrawTextField(MainCanvas _canvas) {
+        // TODO Auto-generated constructor stub
+    }
+
+    /// \ref t16_1 "task 16.1"
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+
+    }
+
     /// \brief on click creates text field at click location
     /// \ref t16_1 "task 16.1"
-	@Override
+    @Override
     public Action handle(Event event) {
         if (!(event instanceof MouseEvent))
             return this;
-        
+
         byte[] array = new byte[7]; // length is bounded by 7
         new Random().nextBytes(array);
         String generatedString = new String(array, Charset.forName("UTF-8"));
-        
-        
+
         MouseEvent mouseEvent = (MouseEvent) event;
         Point2D mousePosition = new Point2D(mouseEvent.getX(), mouseEvent.getY());
         Point2D currentPoint = new Point2D(mouseEvent.getX(), mouseEvent.getY());
-		float x = (float) currentPoint.getX();
-		// Has to subtract current point from 780 because itext7 uses a bottom to top y instead of top to bottom
-		float y = (float) (780.0 - currentPoint.getY());
-		
-		if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
-			
-			PdfDocument pdfDoc = null;
-			try {
-				pdfDoc = new PdfDocument(new PdfWriter(DES).setSmartMode(true));
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
-			PdfDocument srcDoc = null;
-			try {
-				srcDoc = new PdfDocument(new PdfReader(SRC));
-			} catch (IOException e1) {
-				
-				e1.printStackTrace();
-			}
-			srcDoc.copyPagesTo(1, srcDoc.getNumberOfPages(), pdfDoc);
-			
-			Document doc = new Document(pdfDoc);
+        float x = (float) currentPoint.getX();
+        // Has to subtract current point from 780 because itext7 uses a bottom to top y instead of top to bottom
+        float y = (float) (780.0 - currentPoint.getY());
 
-			PdfAcroForm form = PdfAcroForm.getAcroForm(doc.getPdfDocument(), true);
-			PdfTextFormField nameField = PdfTextFormField.createText(doc.getPdfDocument(), new Rectangle(x, y, 150, 15), generatedString, "");
-			form.addField(nameField);
-			pdfDoc.close();
-			srcDoc.close();
-			
-	        File file = new File("src/main/resources/manipulate_pdf/test_pdf.pdf");
+        if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
 
-	        File rename = new File("src/main/resources/manipulate_pdf/test_pdf_old_old.pdf");
+            PdfDocument pdfDoc = null;
+            try {
+                pdfDoc = new PdfDocument(new PdfWriter(DES).setSmartMode(true));
+            } catch (FileNotFoundException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            PdfDocument srcDoc = null;
+            try {
+                srcDoc = new PdfDocument(new PdfReader(SRC));
+            } catch (IOException e1) {
 
-	        boolean flag = file.renameTo(rename);
+                e1.printStackTrace();
+            }
+            srcDoc.copyPagesTo(1, srcDoc.getNumberOfPages(), pdfDoc);
 
-	        if (flag == true) {
-	            System.out.println("File Successfully Rename");
-	        }
+            Document doc = new Document(pdfDoc);
 
-	        else {
-	            System.out.println("Operation Failed");
-	        }
+            PdfAcroForm form = PdfAcroForm.getAcroForm(doc.getPdfDocument(), true);
+            PdfTextFormField nameField = PdfTextFormField.createText(doc.getPdfDocument(), new Rectangle(x, y, 150, 15),
+                    generatedString, "");
+            form.addField(nameField);
+            pdfDoc.close();
+            srcDoc.close();
 
-	        file = new File("src/main/resources/manipulate_pdf/test_pdf_old.pdf");
+            File file = new File("src/main/resources/manipulate_pdf/test_pdf.pdf");
 
-	        rename = new File("src/main/resources/manipulate_pdf/test_pdf.pdf");
-	  
-	        flag = file.renameTo(rename);
+            File rename = new File("src/main/resources/manipulate_pdf/test_pdf_old_old.pdf");
 
-	        if (flag == true) {
-	            System.out.println("File Successfully Rename");
-	        }
+            boolean flag = file.renameTo(rename);
 
-	        else {
-	            System.out.println("Operation Failed");
-	        }
+            if (flag == true) {
+                System.out.println("File Successfully Rename");
+            }
 
-	        file = new File("src/main/resources/manipulate_pdf/test_pdf_old_old.pdf");
+            else {
+                System.out.println("Operation Failed");
+            }
 
-	        rename = new File("src/main/resources/manipulate_pdf/test_pdf_old.pdf");
+            file = new File("src/main/resources/manipulate_pdf/test_pdf_old.pdf");
 
-	        flag = file.renameTo(rename);
+            rename = new File("src/main/resources/manipulate_pdf/test_pdf.pdf");
 
-	        if (flag == true) {
-	            System.out.println("File Successfully Rename");
-	        }
+            flag = file.renameTo(rename);
 
-	        else {
-	            System.out.println("Operation Failed");
-	        }
-			
-		}
-		
+            if (flag == true) {
+                System.out.println("File Successfully Rename");
+            }
+
+            else {
+                System.out.println("Operation Failed");
+            }
+
+            file = new File("src/main/resources/manipulate_pdf/test_pdf_old_old.pdf");
+
+            rename = new File("src/main/resources/manipulate_pdf/test_pdf_old.pdf");
+
+            flag = file.renameTo(rename);
+
+            if (flag == true) {
+                System.out.println("File Successfully Rename");
+            }
+
+            else {
+                System.out.println("Operation Failed");
+            }
+
+        }
+
         return null;
     }
-	/// \ref t16_1 "task 16.1"
-	@Override
-	public boolean isComplete() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	/// \ref t16_1 "task 16.1"
-	@Override
-	public boolean contains(Point2D point) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	/// \ref t16_1 "task 16.1"
-	@Override
-	public void pdfExecute(PdfCanvas canvas, PdfPage page) {
-		// TODO Auto-generated method stub
-		
-	}
+
+    /// \ref t16_1 "task 16.1"
+    @Override
+    public boolean isComplete() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /// \ref t16_1 "task 16.1"
+    @Override
+    public boolean contains(Point2D point) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /// \ref t16_1 "task 16.1"
+    @Override
+    public void pdfExecute(PdfCanvas canvas, PdfPage page) {
+        // TODO Auto-generated method stub
+
+    }
 }
