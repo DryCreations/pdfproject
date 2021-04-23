2d3e9add99557a0cf68b925c3e5ba31341595756
Hunter Gongloff <hgongloff12@gmail.com>
2021-04-22 20:56:05 -0400

    Added bracket

--- src/main/java/com/groupseven/pdfproject/utilities/DrawingTool.java
+++ src/main/java/com/groupseven/pdfproject/utilities/DrawingTool.java
@@ -53,6 +53,7 @@ public class DrawingTool implements EventHandler<MouseEvent> {
 
         if (_currentAction != null)
             _currentAction.handle(event);
+    }
 
     /// \brief set color
     ///
