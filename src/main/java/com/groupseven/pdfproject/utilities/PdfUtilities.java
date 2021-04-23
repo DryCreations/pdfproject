350e1a53f2699acc71100d5a0fbd4ecefecc776f
Cassandra Mae <patel.855@wright.edu>
2021-04-23 11:48:35 -0400

    update doxygen for PdfUtilities

--- src/main/java/com/groupseven/pdfproject/utilities/PdfUtilities.java
+++ src/main/java/com/groupseven/pdfproject/utilities/PdfUtilities.java
@@ -5,12 +5,12 @@ import java.util.List;
 /// This class includes utilities for working with the PDF files
 public class PdfUtilities {
 
-    /// \imp \ref T4_1 Merges a list of PDFs together in the order they were inserted into the list
+    /// \ref t4_1 Merges a list of PDFs together in the order they were inserted into the list
     public void mergeFiles(List<Object> pdfs) {
 
     }
 
-    /// \imp \ref T6_1 Highlights an area of the PDF file
+    /// \ref t6_1 Highlights an area of the PDF file
     public void highlight(Object area) {
 
     }
